cae2b9db627a3ece4c0766f5195aeff88eafb019
Rafael Koh <rafaelkoh97@gmail.com>
2021-08-30 23:46:38 -0500

    connection with mongodb

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
+app.use(express.json());
+
 moviesApi(app);
 
 app.listen(config.port, function(){
